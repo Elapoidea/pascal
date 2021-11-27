22ead0c4cb2822f9117b8b98c61234ba30e3e5ac
Elapoidea <71977382+Elapoidea@users.noreply.github.com>
2021-11-27 17:46:47 +0000

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,17 @@
+fn pascal(rows: usize) -> Vec<Vec<usize>> { 
+    (0..rows).map(|r| {
+        (1..=rows-r).map(|n| {
+            (n..=n+r-1).product::<usize>() / (1..=r).product::<usize>()
+        }).collect()}).collect()}
+
+fn main() {
+    for i in pascal(20) {
+        for j in i { 
+            print!("{}", j); 
+            
+            for _ in 0..8-j.to_string().len() { print!(" ") } 
+        }
+
+        println!();
+    }
+}
